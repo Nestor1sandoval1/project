f53360dee83ce12e4d902f4d3ffe57cac371ce79
Nestor1sandoval1 <77358673+Nestor1sandoval1@users.noreply.github.com>
2021-01-19 20:49:39 +0100

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,11 @@
+$(".part2").hide();
+$(".start").click(function() {
+    $(".intro").hide();
+    $(".part2").show();
+});
+$(".second3").hide();
+$(".killit").click(function(){
+$(".part2").hide();
+$(".").show();
+});
+
